5c49a03c45476d3f258d3af1250aa65c6d5628b8
gabrielSeixas <salesseixasgabriel@gmail.com>
2018-10-22 12:04:04 -0300

    Add some doc

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -16,6 +23,19 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hadn_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -30,5 +50,11 @@ defmodule Cards do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, the_reason} -> the_reason
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+      |> Cards.deal(hand_size)
   end
 end
