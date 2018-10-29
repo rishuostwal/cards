871ada8d5f72beca3efee43fe0ee037407d323b8
gabrielSeixas <salesseixasgabriel@gmail.com>
2018-10-29 15:18:28 -0300

    Add docs

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,4 +1,11 @@
 defmodule Cards.CLI do
+	@moduledoc """
+  Provides the main function for CLI
+	"""
+	
+	@doc """
+	Main function
+	"""
 	def main(_args) do
 		{hand, deck} = Cards.create_hand(5)
 		Cards.Game.game(hand)
